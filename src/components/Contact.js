4030e739124e90281519039c4b867c7d9e641370
Mukesh <mukeshdixena123@gmail.com>
2025-07-08 23:44:22 +0530

    Update Contact component: change Google Form link for the contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
             <h2>Contact Us</h2>
             <p>Have a project in mind? Fill the form and our team will get back to you.</p>
             <a
-                href="https://forms.gle/YOUR_GOOGLE_FORM_LINK"
+                href="https://docs.google.com/forms/d/e/1FAIpQLSfyzY5k-yp8Mqaxp5P6AHRSb3QuniIb2DDI3NYyo9fAwkfs_Q/viewform"
                 target="_blank"
                 rel="noreferrer"
                 className="btn-secondary"
